89a65e4674dd6757d978b6a5af8dc94dcb286ea7
arpit alne <arpitalne619@gmail.com>
2020-09-24 21:47:49 +0530

    whatsapp function

--- script.js
+++ script.js
@@ -1,73 +1,74 @@
-const quotesContainer=document.getElementById("quote-cointainer");
-const quoteText=document.getElementById("quote");
-const authorText=document.getElementById("author");
-const twitterBtn=document.getElementById("twitter");
-const newQuoteBtn=document.getElementById("new-quote");
-const loader=document.getElementById('loader');
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const whatsappBtn = document.getElementById('whatsapp');
+const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
-//show loading
-
-function loading(){
-    loader.hidden=false;
-    quotesContainer.hidden=true;
-
-}
-// hide loading
-function complete(){
-    if (!loader.hidden){
-        quotesContainer.hidden=false;
-        loader.hidden=true;
-    }
+// Loading Spinner Shown
+function loading() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
 }
 
-
-// Get Qutoes from Api
-async function getQuote() {
-    // loading();
-
-    const proxyUrl="https://cors-anywhere.herokuapp.com/"
-    const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
-    try{
-        const response= await fetch(proxyUrl+apiUrl);
-        const data=await response.json();
-        // console.log(data);
-
-        // for blank author
-        if(data.quoteAuthor===''){
-            authorText.innerHTML='Unknown';
-        }else{
-            authorText.innerText=data.quoteAuthor;
-        }
-        //redude font size
-        if(data.quoteText.length>120){
-            quoteText.classList.add('long-quote');
-        }else{
-            quoteText.classList.remove('long-quote');
-            
-        }
-        quoteText.innerText=data.quoteText;
-        console.log(quoteText);
-        console.log(authorText);
-
-        // stop loader and shoe 
-        // complete();
-
-    }catch(error){
-        getQuote();
-        // console.log("oops ","no quotes",error);
-    }
-}
-// tweet
-function tweetQuote() {
-    const quote=quoteText.innerText;
-    const author=authorText.innerText;
-    const twitterUrl=`https://twitter.com/intent/tweet?text=${quote} - ${author}`;
-    window.open(twitterUrl,'_blank');
+// Remove Loading Spinner
+function complete() {
+  if (!loader.hidden) {
+    quoteContainer.hidden = false;
+    loader.hidden = true;
   }
-//event listeners
+}
 
-newQuoteBtn.addEventListener('click',getQuote);
-twitterBtn.addEventListener('click',tweetQuote)
+// Get Quote From API
+async function getQuote() {
+  loading();
+  // We need to use a Proxy URL to make our API call in order to avoid a CORS error
+  const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+  const apiUrl = 'https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
+  try {
+    const response = await fetch(proxyUrl + apiUrl);
+    const data = await response.json();
+    // Check if Author field is blank and replace it with 'Unknown'
+    if (data.quoteAuthor === '') {
+      authorText.innerText = 'Unknown';
+    } else {
+      authorText.innerText = data.quoteAuthor;
+    }
+    // Dynamically reduce font size for long quotes
+    if (data.quoteText.length > 120) {
+      quoteText.classList.add('long-quote');
+    } else {
+      quoteText.classList.remove('long-quote');
+    }
+    quoteText.innerText = data.quoteText;
+    // Stop Loading, Show Quote
+    complete();
+  } catch (error) {
+    getQuote();
+  }
+}
 
-// on load 
-// getQuote();
+// Tweet Quote
+function tweetQuote() {
+  const quote = quoteText.innerText;
+  const author = authorText.innerText;
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
+  window.open(twitterUrl, '_blank');
+}
+function whatsapp(){
+  const quote = quoteText.innerText;
+  const author = authorText.innerText;
+  const whatsappUrl = `whatsapp://send?text=${quote} - ${author}`;
+  window.open(whatsappUrl, '_blank');
+  
+  
+}
+
+// Event Listeners
+newQuoteBtn.addEventListener('click', getQuote);
+twitterBtn.addEventListener('click', tweetQuote);
+whatsappBtn.addEventListener('click', whatsapp);
+
+// On Load
+getQuote();
